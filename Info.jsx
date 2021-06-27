ae05c0b96e78038e32916590f5a4f14b637c7408
Mayankchoudhary2001 <86488171+Mayankchoudhary2001@users.noreply.github.com>
2021-06-27 18:08:04 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from " react";
+
+function Info() {
+  return (
+    <div clsss="note">
+    <h1 clsss="note">Javescript and React.js</h1>
+    <p clsss="note">A basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
